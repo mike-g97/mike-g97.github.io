2841268c5655d22dbc547f45419d024dc065763d
Mikhael E Gonzalez <mikhael.gonzalez@gmail.com>
2019-04-24 23:36:11 -0400

    Updated CODE-ASSESSMENT_Restaurant Site with HTML, CSS, & JavaScript responsiveness.

--- CODE-ASSESSMENT_Restaurant Site with HTML, CSS, & JavaScript/contact.js
+++ CODE-ASSESSMENT_Restaurant Site with HTML, CSS, & JavaScript/contact.js
@@ -1,33 +1,25 @@
-var name = document.getElementById('nameAlert');
-var email = document.getElementById('emailAlert');
-var phone = document.getElementById('phoneAlert');
-var yes = document.getElementById('yesAlert');
-var no = document.getElementById('noAlert');
 
-function yourInquiry () {
-    name.required = true;
-    email.required = true;
-    phone.required = true;
-    yes.required = true;
-    no.required = true;
+
+function submit () {
+    console.log("submit() called");
+    var name = document.getElementById('nameAlert');
+    var email = document.getElementById('emailAlert');
+    var phone = document.getElementById('phoneAlert');
+    var yes = document.getElementById('yesAlert');
+    var no = document.getElementById('noAlert');
 
     if (name.value == false) {
-      console.log("Alert: Input your name please");
+      console.log("name not inputted");
       // alert("Input your name please");
     }
     if (email.value == false) {
-      console.log("Alert: which email is best to contact you");
+      console.log("email not inputted");
       // alert("which email is best to contact you");
     }
     if (phone.value == false) {
-      console.log("Alert: write a vaild phone number; no dashes");
+      console.log("phone not inputted");
       // alert("write a vaild phone number; no dashes");
-    }
-    if (yes.value == false && no.value == false) {
-      console.log("Alert: we would like to know if you have been here before, to better assist you with your inquiry");
-      // alert("we would like to know if you have been here before, to better assist you with your inquiry");
-    }
-    else {
+    }else {
       console.log("inputs were made");
     }
 }
